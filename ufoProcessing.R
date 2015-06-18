eed5d5a4d3c2439c39b50aefa5363f1e90187839
Eric Milliman <emilliman4@gmail.com>
2015-06-18 17:11:21 -0400

    cleaned and made initial data processing more uniform.

--- ufoProcessing.R
+++ ufoProcessing.R
@@ -19,12 +19,13 @@ ufos <- htmlParse(ufos, encoding = "UTF-8")
 titles  <- xpathSApply (ufos ,"//a/@href") 
 titles<-titles[c(-1,-length(titles))]
 
-tables <- mclapply(titles, mc.cores = 10, function(x) readHTMLTable(paste0(base,x)))
+tables <- mclapply(titles, mc.cores = 4, function(x) readHTMLTable(paste0(base,x)))
 obs<-do.call(rbind, lapply(tables, as.data.frame))
 free(ufos)
 
 colnames(obs)<-gsub("NULL.","", colnames(obs))
-obs[,"Date"]<-as.Date(do.call(rbind,strsplit(as.character(obs$Date...Time), " "))[,1], "%m/%d/%Y")
+obs<-obs[-which(obs$Date...Time == ""),]
+obs[,"Date"]<-as.Date(do.call(rbind,strsplit(as.character(obs$Date...Time), " "))[,1], "%m/%d/%y")
 
 obs<-obs[,c(8,2,3,4,5,6)]
 obs$City<-as.character(obs$City)
@@ -33,7 +34,7 @@ obs$City<-gsub("[/\\(].*","",obs$City, perl=T)
 obs$locale<-paste(obs$City, obs$State, sep=",")
 obs$locale<-gsub("\\\"","", obs$locale)
 
-write.table(obs, "UFO_sightings.txt", col.names=T, row.names = F, sep="\t", quote=T)
+write.csv(obs, "UFO_sightings.txt", col.names = T, row.names = F, quote=T)
 
 locations<-unique(obs$locale)
 
@@ -49,14 +50,14 @@ geocodes<-as.data.frame(geocodes)
 geocodes$query<-rownames(geocodes)
 rownames(geocodes)<-seq(1,length(geocodes[,1]))
 
-write.table(geocodes[,c(3,1,2)], "geocodes_DSTK.txt", sep="\t", quote = F, col.names = T, row.names=F)
+write.csv(geocodes[,c(3,1,2)], "geocodes_DSTK.txt", quote = T, col.names = T, row.names=F)
 
 obs.summary<-count(obs, "locale")
 obs.summary<-merge(obs.summary, geocodes, by.x="locale", by.y="query", all.x=T)
 obs.summary$long<-as.character(obs.summary$long)
 obs.summary$lat<-as.character(obs.summary$lat)
 
-write.table(obs.summary, "UFO_aggregate_sightings.txt", col.names = T, row.names = F)
+write.csv(obs.summary, "UFO_aggregate_sightings.txt", quote=T, col.names = T, row.names = F)
 
 
 ##Google Maps API call, limited to 2500 requests per 24hrs.
