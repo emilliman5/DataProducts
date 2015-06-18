d3abd1eea6b41baa7050b9e10043f5e1b3851799
Eric Milliman <emilliman4@gmail.com>
2015-06-18 11:26:21 -0400

    Added slider for radius input. added link to DSTK site

--- ui.R
+++ ui.R
@@ -6,6 +6,7 @@ shinyUI(pageWithSidebar(
     h2("Enter a location:"),
     numericInput("lat","Latitude", 45.0, min=25, max=90),
     numericInput("long", "Longitude", -100, min=-126, max=-60),
+    sliderInput("rad", "Radius (mi)", min=1, max=500, value=150),
     submitButton("Submit"),
     h2("Sightings in your vicinity"),
     h4("You entered:"),
@@ -16,6 +17,6 @@ shinyUI(pageWithSidebar(
   mainPanel(
     plotOutput("map"),
     h3("Acknowledgements"),
-    p('UFO sightings data was obtained from the National UFO reporting center (http://www.nuforc.org). The locations from the sightings data was gecoded using the DataScientists ToolKit () and Google Maps API servie and ggmap, D. Kahle and H. Wickham. ggmap: Spatial Visualization with ggplot2. The R Journal,
+    p('UFO sightings data was obtained from the National UFO reporting center (http://www.nuforc.org). The locations from the sightings data was gecoded using the DataScientists ToolKit (http://www.datasciencetoolkit.org) and Google Maps API servie and ggmap, D. Kahle and H. Wickham. ggmap: Spatial Visualization with ggplot2. The R Journal,
     5(1), 144-161. URL http://journal.r-project.org/archive/2013-1/kahle-wickham.pdf'))
 ))
